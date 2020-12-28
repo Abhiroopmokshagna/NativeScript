68e6e7ed1403007c989fd92a7913d95e9be8b28e
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2020-12-28 16:00:43 +0530

    menulist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
+import { NativeScriptHttpClientModule } from '@nativescript/angular';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
+import { MenuComponent } from "./menu/menu.component";
+import { DishService } from "./services/dish.service";
+import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
+import { baseURL } from './shared/baseurl';
 @NgModule({
     bootstrap: [
         AppComponent
     ],
     imports: [
         NativeScriptModule,
+        NativeScriptHttpClientModule,
         AppRoutingModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        MenuComponent
     ],
-    providers: [],
+    providers: [
+        {provide: 'baseURL', useValue: baseURL},
+        DishService,
+        ProcessHTTPMsgService
+    ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
